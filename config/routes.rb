9305b8683f1c97ad222e1a2249f8ff49bc64380c
Jordane Celet <mrjocte@gmail.com>
2021-03-09 17:05:36 +0100

    routes init

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get "/preferences", to: "pages#preferences"
+  post "/preferences", to: "pages#submit_preferences"
+
+  resources :menus, only: [:new, :show, :create]
+
+  resources :recipes, only: [:show] do
+    member do
+      get "/instructions", to: "recipes#instructions"
+    end
+  end
+
+  resources :recipes_preferences, only: [:create]
+
+  get "/shoppinglist", to: "user_ingredients#index"
+  get "/dashboard", to: "pages#dashboard"
 end
