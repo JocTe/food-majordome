ca3a0da2e15b41af03432f423c2da9897d2f4e35
Alexis Bouchiounane <alexis.bouchiouane@gmail.com>
2021-03-11 15:00:54 +0100

    dashboard construction

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,20 +4,23 @@ class PagesController < ApplicationController
   def home
   end
 
-  def preferences 
+  def dashboard
+  end
+
+  def preferences
     session["preferences"] = { number_of_people: 1, vegan: false, vegetarian: false, gluten_free: false, dairy_free: false, pescetarian: false, no_diet: false, ingredients_disliked: [], ingredients_wanted: [] }
   end
 
   def submit_preferences
-    preferences = session["preferences"]    
+    preferences = session["preferences"]
     params["preferences"].each do |key, value|
       if preferences.key?(key)
         preferences[key] = value
       end
-    end    
+    end
     # enregistrer les infos dans la session
     # updtater les preferences
     render json: session["preferences"].to_json
   end
-  
+
 end
