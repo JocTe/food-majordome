9fbd5a2df3824a553e045e610672df129b4d1263
Jordane Celet <mrjocte@gmail.com>
2021-03-11 15:24:32 +0100

    Removed type from ingredients table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'open-uri'
+require 'json'
 
 
 
 def fetchRecipes(args = {})
     amount = args[:amount]
+    diet = args[:diet]
     api_key = ENV['SPOONACULAR_KEY']
-    url = "https://api.spoonacular.com/recipes/random?apiKey=#{api_key}&number=#{amount}"
+    url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=ed3b814f3a1f4eb8b9236d2fd37a8425&diet=#{diet}&instructionsRequired=true&addRecipeInformation=true&addRecipeNutrition=true&ignorePantry=true&sort=meta-score&sortDirection=desc&type=main course,salad,soup,side dish&number=#{amount}"
     recipes_serialized = open(url).read
     recipes = JSON.parse(recipes_serialized)
+    recipes["results"]
 end
+# puts JSON.pretty_generate(fetchRecipes({amount: 2, diet: "vegetarian"}))
 
-Recipe.create!(servings: 2, prep_time:20, score:63.2, health_score:68, calories: 564, name: "Test", summary: "This is a test recipe", image:"https://spoonacular.com/cdn/ingredients_100x100/pineapple.jpg", cheap: true, dairy_free: true, gluten_free: false, vegan: false, vegetarian: false, healthy: true, pescetarian: true, cuisine: "French", author: "me", spoonacular_id:5665)
+def createRecipe(args = {})
+    fetchRecipes(args).each do |recipe|
+        recipe["ingredients"].each do |ingedient|
+            Ingredient.new(calories: ingedient["nutrients"][1]["amount"], name: ingedient["name"])
+        end
+    end
+end
+# Recipe.create!(servings: 2, prep_time:20, score:63.2, health_score:68, calories: 564, name: "Test", summary: "This is a test recipe", image:"https://spoonacular.com/cdn/ingredients_100x100/pineapple.jpg", cheap: true, dairy_free: true, gluten_free: false, vegan: false, vegetarian: false, healthy: true, pescetarian: true, cuisine: "French", author: "me", spoonacular_id:5665)
 
 
 
