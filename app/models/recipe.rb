b2bc2e5c7513d88886a2aec4f5706f2f2cfe3c84
Jordane Celet <mrjocte@gmail.com>
2021-03-11 21:08:05 +0100

    seeds working but issued to find ingredient image

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
-  has_many :proportions
-  has_many :steps
+  has_many :proportions, dependent: :destroy
+  has_many :steps, dependent: :destroy
   has_many :ingredients, through: :proportions
   has_many :reviews
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,12 @@
 require 'open-uri'
 require 'json'
 
-
+puts "-----cleaning the DB-----"
+Recipe.destroy_all
+Ingredient.destroy_all
+Step.destroy_all
+Proportion.destroy_all
+puts "-------DB cleaned-----"
 
 def fetch_recipes_api(args = {})
     amount = args[:amount]
@@ -34,38 +39,43 @@ def create_recipes_and_associations(args = {})
     puts "Will be creating #{args[:amount]} Recipe of #{args[:diet]} food"
     fetch_recipes_api(args).each do |recipe|
 
-        puts "Creating Recipe name : #{recipe["title"]}"
+        unless recipe["analyzedInstructions"].empty?
+                
+            puts "Creating Recipe name : #{recipe["title"]}"
 
-        new_recipe = Recipe.create!(servings: recipe["servings"], prep_time: recipe["readyInMinutes"], score: recipe["spoonacularScore"],
-            health_score: recipe["healthScore"], calories: recipe["nutrition"]["nutrients"][0]["amount"], name: recipe["title"], 
-            summary: recipe["summary"], image: recipe["image"], cheap: recipe["cheap"], dairy_free: recipe["dairyFree"], gluten_free: recipe["glutenFree"],
-            vegan: recipe["vegan"], vegetarian: recipe["vegetarian"], healthy: recipe["veryHealthy"], cuisines: recipe["cuisines"], diets: recipe["diets"], 
-            author: recipe["creditsText"], spoonacular_id: recipe["id"]
-            )
-        puts "--------------------------------------------------------------"
+            new_recipe = Recipe.create!(servings: recipe["servings"], prep_time: recipe["readyInMinutes"], score: recipe["spoonacularScore"],
+                health_score: recipe["healthScore"], calories: recipe["nutrition"]["nutrients"][0]["amount"], name: recipe["title"], 
+                summary: recipe["summary"], image: recipe["image"], cheap: recipe["cheap"], dairy_free: recipe["dairyFree"], gluten_free: recipe["glutenFree"],
+                vegan: recipe["vegan"], vegetarian: recipe["vegetarian"], healthy: recipe["veryHealthy"], cuisines: recipe["cuisines"], diets: recipe["diets"], 
+                author: recipe["creditsText"], spoonacular_id: recipe["id"]
+                )
+            puts "--------------------------------------------------------------"
 
-        puts "Creating Steps for : #{recipe["title"]}"
+            puts "Creating Steps for : #{recipe["title"]}"
 
-        creating_steps_of_recipe(recipe["analyzedInstructions"][0]["steps"], new_recipe)
+            creating_steps_of_recipe(recipe["analyzedInstructions"][0]["steps"], new_recipe)
 
-        puts "Steps done"
+            puts "Steps done"
 
-        puts "-----------------------------------------------------------------"
+            puts "-----------------------------------------------------------------"
 
 
-        puts "Creating Ingredients"
+            puts "Creating Ingredients"
 
-        recipe["nutrition"]["ingredients"].each do |ingredient|
-            # check if ingredient exist in the database
-            if Ingredient.find_by(name: ingredient["name"])
-                i = Ingredient.find_by(name: ingredient["name"])
-            else
-                i = Ingredient.create!(name: ingredient["name"], image: "#{ingredient["name"]}.jpg")
+            recipe["nutrition"]["ingredients"].each do |ingredient|
+                # check if ingredient exist in the database
+                if Ingredient.find_by(name: ingredient["name"])
+                    i = Ingredient.find_by(name: ingredient["name"])
+                    puts "FOUND : #{ingredient["name"]}"
+                else
+                    i = Ingredient.create!(name: ingredient["name"], image: "#{ingredient["name"].gsub(' ','-')}")
+                    puts "NOT FOUND : #{ingredient["name"]} so it was created"
+                end
+                proportion = Proportion.new(unit: ingredient["unit"], amount: ingredient["amount"])
+                proportion.recipe = new_recipe
+                proportion.ingredient = i
+                proportion.save!
             end
-            proportion = Proportion.new(unit: ingredient["unit"], amount: ingredient["amount"])
-            proportion.recipe = new_recipe
-            proportion.ingredient = i
-            proportion.save!
         end
     end
 end
@@ -75,7 +85,10 @@ end
 
 create_recipes_and_associations(amount: 100, diet: "vegetarian")
 create_recipes_and_associations(amount: 100, diet: "vegan")
-create_recipes_and_associations(amount: 100, diet: "vegan")
+create_recipes_and_associations(amount: 100, diet: "pescetarian")
+create_recipes_and_associations(amount: 100, diet: "gluten free")
+create_recipes_and_associations(amount: 100, diet: "omnivore")
+
 
 
 # Recipe.create!(servings: 2, prep_time:20, score:63.2, health_score:68, calories: 564, name: "Test", summary: "This is a test recipe", image:"https://spoonacular.com/cdn/ingredients_100x100/pineapple.jpg", cheap: true, dairy_free: true, gluten_free: false, vegan: false, vegetarian: false, healthy: true, pescetarian: true, cuisine: "French", author: "me", spoonacular_id:5665)
