2948db5e14313f987017cac66b85c4874231dade
Jordane Celet <mrjocte@gmail.com>
2021-03-11 23:34:07 +0100

    ADDED: recipe are displayed randomly

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,11 @@
 class MenusController < ApplicationController
+    skip_before_action :authenticate_user!, only: [ :new ]
+
+    def new
+        @menu = Menu.new
+        @suggestions = Recipe.all.sample(5) # where preferences match get last 5
+        
+        authorize @menu
+    end
+
 end
