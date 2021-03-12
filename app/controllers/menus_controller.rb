2ebec67c5deab1aa1cc8937cdb0ed92b5da503e7
Alexis Bouchiounane <alexis.bouchiouane@gmail.com>
2021-03-12 12:17:02 +0100

    controller menu + show

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,35 @@
 class MenusController < ApplicationController
+
+  before_action :set_user, only: [:show, :new, :create]
+  before_action :set_menu, only: [:show]
+
+  def show
+    @menu = Menu.new
+    authorize @menu
+  end
+
+  def new
+    @menu = Menu.new
+    authorize @menu
+  end
+
+  def create
+    # create there ?
+  end
+
+
+
+ private
+  def menu_params
+    params.require(:menu).permit()
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  def set_menu
+    @menu = Menu.find(params[:id])
+    authorize @menu
+  end
 end
