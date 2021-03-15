978c75c8de8d4a1a60a8fffc7f8033b049b70c7a
Jordane Celet <mrjocte@gmail.com>
2021-03-15 21:48:38 +0100

    Added admin page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_200936) do
+ActiveRecord::Schema.define(version: 2021_03_15_203759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_200936) do
     t.string "diet"
     t.integer "number_of_people"
     t.string "budget"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
