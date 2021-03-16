ec18d7b402b37191df5c9a1d8d153383f7e77dbe
Alexis Bouchiounane <alexis.bouchiouane@gmail.com>
2021-03-16 16:32:53 +0100

    swipe

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'rails_admin'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.12.0'
 gem 'simple_form'
+gem 'hammerjs-rails'
 
 group :development, :test do  gem 'pry-byebug'
   gem 'pry-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -31,6 +30,7 @@ import { addDiet } from './add_diet';
 import { addNumberOfPeople } from './add_number_of_people';
 import { initSaveMenu } from "./saveMenu";
 import { loadingScreenTyped } from '../components/loadingScreen';
+import { Carousel } from '../components/carousel';
 
 // import { addIngredientsDisliked } from './add_ingredients_disliked';
 // import { addIngredientsWanted } from './add_ingredients_wanted';
@@ -43,12 +43,13 @@ document.addEventListener('turbolinks:load', () => {
   addNumberOfPeople();
   addDiet();
   initNavbarOnSelection();
-  initNavbarOnScroll();  
+  initNavbarOnScroll();
   initSaveMenu();
   // addIngredientsDisliked();
   // addIngredientsWanted();
   loadingScreenTyped();
   // initSelect2();
+  // appeler Class Carousel ???
 });
 
 import "controllers";
