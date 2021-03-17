4e5c4555643420afa550b591196d6e680808fb36
Jordane Celet <mrjocte@gmail.com>
2021-03-18 00:36:06 +0100

    small adjustement on the loadind

--- app/javascript/components/loadingScreen.js
+++ app/javascript/components/loadingScreen.js
@@ -4,15 +4,19 @@ import Typed from 'typed.js';
 
 const loadingScreenTyped = () => {
     const loadingText = document.getElementById("loading-title");
+
+
     var options = {
-        strings: ['Recolting fresh <strong>ingredients</strong>', 'Getting our hands dirty'],
+        strings: ['Recolting fresh <strong>ingredients</strong>^650 ', 'Filling your shopping list^650', 'Getting our hands <i>dirty</i>^650'],
         typeSpeed: 40,
         cursorChar: '',
+        fadeOut: true,
         loop: true
     };
 
+
     if (loadingText) {
-        var typed = new Typed(loadingText, options);
+        new Typed(loadingText, options);
     }
 }
 
