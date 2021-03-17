430f2e8978980decf3cfabd9eb5217e01a593204
Jordane Celet <mrjocte@gmail.com>
2021-03-17 17:28:10 +0100

    Added a selector for categories

--- app/javascript/components/navCategories.js
+++ app/javascript/components/navCategories.js
@@ -1,33 +1,39 @@
 const initNavCategories = () => {
     const categoriesNav = document.querySelector('.slick-categories-shopping')
+    const categoriesTitles = document.querySelectorAll('.slick-categories-shopping > h5')
     if (categoriesNav) {
-        $(categoriesNav).slick({
-            centerMode: true,
-            slidesToShow: 3,
-            arrows: true,
-            variableWidth: true,
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        arrows: true,
-                        centerMode: true,
-                        centerPadding: '30px',
-                        slidesToShow: 3
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        arrows: true,
-                        centerMode: true,
-                        centerPadding: '30px',
-                        slidesToShow: 3
-                    }
-                }
-            ]
-        });
+        slickBehavior(categoriesNav);
+        categoriesNav.
     }
+}
+
+const slickBehavior = (nav) => {
+    $(nav).slick({
+        centerMode: true,
+        slidesToShow: 3,
+        arrows: true,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '30px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '30px',
+                    slidesToShow: 3
+                }
+            }
+        ]
+    });
 }
 
 export { initNavCategories };
