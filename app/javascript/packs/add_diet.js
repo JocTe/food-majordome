b8a91ac54cc84d1a1a40ed8fd0d5ef923e465965
Jordane Celet <mrjocte@gmail.com>
2021-03-18 15:53:29 +0100

    Removed console in add_diet.js

--- app/javascript/packs/add_diet.js
+++ app/javascript/packs/add_diet.js
@@ -7,17 +7,17 @@ const addDiet = () => {
 
   if (diets) {
     diets.forEach((diet) => {
-  
+
       diet.addEventListener('click', (event) => {
-        
+
         if (event.currentTarget.dataset.state == "false") {
           event.currentTarget.dataset.state = "true";
         } else {
           event.currentTarget.dataset.state = "false";
         };
-  
+
         event.currentTarget.classList.toggle('selected');
-        
+
         const diet = event.currentTarget.dataset.diet;
         const state = event.currentTarget.dataset.state
         const body = {
@@ -26,20 +26,16 @@ const addDiet = () => {
         fetchWithToken(url, {
           method: "POST",
           headers: {
-              "Accept": "application/json",
-              "Content-Type": "application/json"
+            "Accept": "application/json",
+            "Content-Type": "application/json"
           },
           body: JSON.stringify(body),
-         })
-         .then(response => response.json())
-         .then((data) => {           
-           console.log(data);
-         });
-  
+        })
+          .then(response => response.json())
       });
-  
+
     });
-  }  
+  }
 };
 
 export { addDiet }
